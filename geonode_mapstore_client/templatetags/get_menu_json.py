c1c2e1a13e80f19fd3dd9a5e4f41b136a5e8d1b0
Suren <dsuren1@gmail.com>
2025-06-19 15:32:20 +0530

    Enhance navbar left and right menu items (#2093)

--- geonode_mapstore_client/templatetags/get_menu_json.py
+++ geonode_mapstore_client/templatetags/get_menu_json.py
@@ -1,16 +1,8 @@
-from avatar.templatetags.avatar_tags import avatar_url
 from django import template
 from django.conf import settings
 from geonode.base.models import Configuration, Menu, MenuItem
 
 register = template.Library()
-
-
-def _get_request_user(context):
-    request = context.get("request")
-    if request:
-        return request.user
-
 
 def _handle_single_item(menu_item):
     m_item = {}
@@ -20,150 +12,6 @@ def _handle_single_item(menu_item):
     if menu_item.blank_target:
         m_item["target"] = "_blank"
     return m_item
-
-
-def _is_mobile_device(context):
-    if context and "request" in context:
-        req = context["request"]
-        return req.user_agent.is_mobile
-    return False
-
-@register.simple_tag
-def get_brand_navbar_menu():
-    return [
-        {
-            "type": "link",
-            "href": "/all",
-            "label": "All Resources",
-        },
-        {
-            "type": "link",
-            "href": "/datasets",
-            "label": "Datasets",
-        },
-        {
-            "type": "link",
-            "href": "/documents",
-            "label": "Documents",
-        },
-        {
-            "type": "link", 
-            "href": "/maps", 
-            "label": "Maps"
-        },
-        {
-            "type": "link",
-            "href": "/geostories",
-            "label": "GeoStories",
-        },
-        {
-            "type": "link",
-            "href": "/dashboards",
-            "label": "Dashboards",
-        }
-    ]
-
-@register.simple_tag(takes_context=True)
-def get_user_menu(context):
-    is_mobile = _is_mobile_device(context)
-
-    user = _get_request_user(context)
-
-    if not user or (user and not user.is_authenticated):
-        return [
-            {"label": "Register", "type": "link", "href": "/account/signup/?next=/"}
-            if settings.ACCOUNT_OPEN_SIGNUP and not Configuration.load().read_only
-            else None,
-            {"label": "Sign in", "type": "link", "href": "/account/login/?next=/"},
-        ]
-
-    devider = {"type": "divider"}
-
-    profile_link = {
-        "type": "link",
-        # get href of user profile
-        "href": user.get_absolute_url(),
-        "label": "Profile",
-    }
-
-    logout = {"type": "link", "href": "/account/logout/?next=/", "label": "Log out"}
-
-    if is_mobile:
-        return [
-            {
-                # get src of user avatar
-                "image": avatar_url(user),
-                "type": "dropdown",
-                "className": "gn-user-menu-dropdown",
-                "items": [profile_link, devider, logout],
-            }
-        ]
-
-    profile = {
-        # get src of user avatar
-        "image": avatar_url(user),
-        "type": "dropdown",
-        "className": "gn-user-menu-dropdown",
-        "items": [
-            profile_link,
-            {
-                "type": "link",
-                "href": "/social/recent-activity",
-                "label": "Recent activity",
-            },
-            {
-                "type": "link",
-                "href": "/catalogue/search/#/?f=favorite",
-                "label": "Favorites",
-            },
-            devider,
-        ],
-    }
-
-    people_groups = [
-        {"type": "link", "href": "/people/", "label": "People"},
-        {"type": "link", "href": "/groups/", "label": "Groups"},
-        devider
-        ]
-        
-    if user.is_superuser: 
-        people_groups.extend(
-            [
-            {"type": "link", "href": "/invitations/geonode-send-invite/", "label": "Invite users"},
-            {"type": "link","href": "/admin/people/profile/add/","label": "Add user"},
-            {"type": "link", "href": "/groups/create/", "label": "Create group"},
-            devider
-            ]
-        )
-    
-    general =  (
-        people_groups + [
-            {"type": "link", "href": "/help/", "label": "Help"}, 
-            devider, 
-            logout
-            ]
-    )
-
-    admin_only = (
-        [
-            {"type": "link", "href": "/admin/", "label": "Admin"},
-            {
-                "type": "link",
-                "href": settings.GEOSERVER_WEB_UI_LOCATION,
-                "label": "GeoServer",
-            },
-        ]
-        + [devider]
-        + general
-    )
-
-    if user.is_superuser:
-        profile["items"].extend(admin_only)
-    else:
-        profile["items"].extend(general)
-
-    return [profile]
-
 
 @register.simple_tag
 def get_menu_json(placeholder_name):
@@ -187,3 +35,11 @@ def get_menu_json(placeholder_name):
             m = _handle_single_item(menu_items.first())
             ms.append(m)
     return ms
+
+@register.simple_tag
+def get_settings():
+    return {
+        'ACCOUNT_OPEN_SIGNUP': settings.ACCOUNT_OPEN_SIGNUP,
+        'READ_ONLY': Configuration.load().read_only,
+        'GEOSERVER_WEB_UI_LOCATION': settings.GEOSERVER_WEB_UI_LOCATION
+    }
