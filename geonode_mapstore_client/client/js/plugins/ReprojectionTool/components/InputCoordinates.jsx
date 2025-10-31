f94c1389228edc2b410fdb03cf2690ac2d1c33a0
stefanocudini <stefano.cudini@gmail.com>
2025-10-31 10:59:38 +0100

    fix coordinates aligns

--- geonode_mapstore_client/client/js/plugins/ReprojectionTool/components/InputCoordinates.jsx
+++ geonode_mapstore_client/client/js/plugins/ReprojectionTool/components/InputCoordinates.jsx
@@ -8,16 +8,11 @@
 import React, { useState } from 'react';
 import { Form, FormGroup, ControlLabel, InputGroup } from 'react-bootstrap';
 import Message from '@mapstore/framework/components/I18N/Message';
-import { Glyphicon as GlyphiconRB } from 'react-bootstrap';
-import tooltip from '@mapstore/framework/components/misc/enhancers/tooltip';
-const Glyphicon = tooltip(GlyphiconRB);
 
-import CoordinateEntry from '@mapstore/framework/components/misc/coordinateeditors/CoordinateEntry';
 import CoordinatesEditor from '@mapstore/framework/components/mapcontrols/annotations/CoordinatesEditor';
 
 const InputCoordinates = ({
     coordinates = [],
-    //coordinates = [{x:11.1, y:46.1},{x:11.2, y:46.2}],
     format = 'decimal',
     onChange = () => {},
 }) => {
@@ -25,26 +20,13 @@ const InputCoordinates = ({
 
     const componentsValidation = {
         "MultiPoint": {
-            min: 1,
+            min: 1, //specific case min is 1
             add: true,
             remove: true, 
             validation: "validateCoordinates",
             notValid: "annotations.editor.notValidPolyline"
         }
     }
-    // function handleChange(idx, type, newCoord) {
-    //     const updatedCoords = currentCoords.map((coord, cIdx) => {
-    //         if (cIdx === idx) {
-    //             return {
-    //                 ...coord,
-    //                 [type]: parseFloat(newCoord)
-    //             };
-    //         }
-    //         return coord;
-    //     });
-    //     setCurrentCoords(updatedCoords);
-    //     onChange(updatedCoords);
-    // }
 
     const validateLonLat = coord => {
         return coord && 
@@ -56,31 +38,6 @@ const InputCoordinates = ({
 
     return (
         <Form className="reprojection-coordinates" style={{ display: 'flex', gap: '5px' }}>
-            {/* <InputGroup>
-                <InputGroup.Addon style={{ height: '20px' }}>
-                    <Message msgId="latitude"/>
-                </InputGroup.Addon>
-                <CoordinateEntry
-                    idx={0}
-                    format={format}
-                    coordinate="lat"
-                    value={currentCoords[0].y}
-                    onChange={(dd) => handleChange(0, 'y', dd)}
-                />
-            </InputGroup>
-            <InputGroup>
-                <InputGroup.Addon>
-                    <Message msgId="longitude"/>
-                </InputGroup.Addon>
-                <CoordinateEntry
-                    idx={0}
-                    format={format}
-                    coordinate="lon"
-                    value={currentCoords[0].x}
-                    onChange={(dd) => handleChange(0, 'x', dd)}
-                    onKeyDown={() => {}}
-                />
-            </InputGroup> */}
             <InputGroup>
                 <CoordinatesEditor
                     type="MultiPoint"
