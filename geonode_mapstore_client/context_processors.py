81d7f6c50a705a118426bdfc55e7deacf195c863
stefanocudini <stefano.cudini@gmail.com>
2026-08-03 18:44:41 +0200

    update-docs

--- geonode_mapstore_client/context_processors.py
+++ geonode_mapstore_client/context_processors.py
@@ -56,6 +56,7 @@ def resource_urls(request):
         ),
         "ALLOWED_DOCUMENT_TYPES": getattr(settings, "ALLOWED_DOCUMENT_TYPES", []),
         "LANGUAGES": getattr(settings, "LANGUAGES", []),
+        "WMS_MAX_URL_LENGTH": getattr(settings, "WMS_MAX_URL_LENGTH", None),
         "TRANSLATIONS_PATH": getattr(
             settings,
             "MAPSTORE_TRANSLATIONS_PATH",
